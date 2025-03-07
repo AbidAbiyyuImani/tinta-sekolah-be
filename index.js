6e2c53995b1649f42ec872461cfff39c95dba36d
Abid Abiyyu Imani <abidabiyyuimani@gmail.com>
2025-03-08 00:57:54 +0700

    edit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, () => {
-  console.log("Server is running on http://localhost:3794")
+  console.log(`Server is running on http://localhost:${port}`)
 })
 
 app.get("/", (request, response) => {
